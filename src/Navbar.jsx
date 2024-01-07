331d4d1874a5c13ad59ac7e8f8fe6052a6efe7aa
Rajat <rajat.prajapati.che20@itbhu.ac.in>
2024-01-08 00:24:58 +0530

    change navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" style={{fontWeight: "700" }} href="#">
+          <a class="navbar-brand ms-4" style={{fontWeight: "700" }} href="#">
             Mode UI
           </a>
           <button
@@ -22,30 +22,16 @@ const Navbar = () => {
           </button>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav m-auto p-2">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item dropdown">
+            <li class="nav-item dropdown">
                 <a
                   class="nav-link dropdown-toggle"
                   href="#"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
+                  style={{color: "#363C46", fontWeight: "600"}}
                 >
-                  Dropdown link
+                  Card access
                 </a>
                 <ul class="dropdown-menu">
                   <li>
@@ -65,7 +51,36 @@ const Navbar = () => {
                   </li>
                 </ul>
               </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#" style={{color: "#363C46", fontWeight: "600"}}>
+                  Banking
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{color: "#363C46", fontWeight: "600"}}>
+                  Processing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{color: "#363C46", fontWeight: "600"}}>
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{color: "#363C46", fontWeight: "600"}}>
+                  Career
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{color: "#363C46", fontWeight: "600"}}>
+                  Contact
+                </a>
+              </li>
             </ul>
+          </div>
+          <div className="me-3 px-2 border border-dark rounded" style={{backgroundColor: "#582066", color: "#ffffff"}}>
+            Login &nbsp;
+            <i class="fa-solid fa-arrow-right-to-bracket"></i>
           </div>
         </div>
       </nav>
